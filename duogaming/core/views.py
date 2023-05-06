6315901238ebfb0009ab9b0a419d20d2c4f8b8ea
Benjamin Schindler <esgandor@gmail.com>
2023-05-06 16:53:51 -0400

    Crear y modificar index

--- duogaming/core/views.py
+++ duogaming/core/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from games.models import Categoria, Juego
 # Create your views here.
 
 def index(request):
-    return render(request, "core/index.html")
+    juegos = Juego.objects.all()
+    categorias = Categoria.objects.all()
+
+
+    return render(request, "core/index.html", {
+        # diccionario que pasa las listas creadas anteriormente 
+        # como parameters al render
+        'categorias': categorias,
+        'juegos': juegos,
+    })
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
---
+++ duogaming/games/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('nombre',)
+    def __str__(self):
+        return self.nombre
+
+class Juego(models.Model):
+    categoria = models.ForeignKey(Categoria, related_name = 'juegos', on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField(blank=True, null=True)
+    precio = models.FloatField()
+    imagen = models.ImageField(upload_to='juego_imagenes', blank=True, null=True)
+
+    class Meta:
+        ordering=('nombre', )
+
+    def __str__(self):
+        return self.nombre
+
+    
