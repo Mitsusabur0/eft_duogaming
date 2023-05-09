b8c61c806bd3bab6a38ed47e31125f2db407fa83
Giovannicastroduoc <114882120+Giovannicastroduoc@users.noreply.github.com>
2023-05-08 22:02:40 -0400

    En admin se agrega:filtro,busqueda por nombre y listado

--- duogaming/games/admin.py
+++ duogaming/games/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Categoria, Juego
 # Register your models here.
 
+class JuegoAdmin(admin.ModelAdmin):
+    list_display = ["nombre","precio","categoria"]
+    list_editable = ["precio"]
+    search_fields = ["nombre"]
+    list_filter = ["categoria"]
 
 admin.site.register(Categoria)
-admin.site.register(Juego)
+admin.site.register(Juego,JuegoAdmin)
